a078f24a3904b4004eb7779d0565361fa6883a94
labiybafakh <labiybafakh@gmail.com>
2026-01-20 02:50:45 +0900

    [fix]: Add compatibility for linux

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -1,5 +1,12 @@
+#ifdef __APPLE__
 #include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
 #include <GLUT/glut.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
 #include <iostream>
 #include <vector>
 #include <sys/socket.h>
